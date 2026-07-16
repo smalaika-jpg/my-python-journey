3495ba768d309cf07abf786a49419510db4bf19e
smalaika-jpg <smalaika0609@gmail.com>
2026-07-17 00:30:54 +0500

    upload raw grocery list project
    My very first try in writing my own python projects.This has many bugs but its my first initial step,

---
+++ raw_grocery_list_project.py
@@ -0,0 +1,15 @@
+                         GROCERY LIST
+print= input("Enter your total budget")
+list_items= {Chicken,Milk,Cheese,Rice,Yogurt,Butter,Chocolate,Vegetables,Mint,Coriander Leaves,Bread"}
+print("Final checking of items")
+if list_items are:
+   print(f"this is the list items:" {list_items})
+   print("Great job buddy")
+elif:
+   Additinal_items= input("Enter the name of your additional items)
+    new_list= list_items.append(Additional_items)
+   Additional_items_price= input("Enter the price of your addditional items")
+ New_budget= Budget+("Additional_items_price")
+print=("Are you fine with the new budget?")
+print("Say yes if you are or else please remove the addtional items")
+     print("Happy Shopping")
