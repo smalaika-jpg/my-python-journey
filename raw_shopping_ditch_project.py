436edbd698d698391adecce3f2715d20cb0c90e4
smalaika-jpg <smalaika0609@gmail.com>
2026-07-17 20:29:49 +0500

    upload my raw shopping ditch project
    this is just an idea of using python in daily life conversation

---
+++ raw_shopping_ditch_project.py
@@ -0,0 +1,11 @@
+           Shopping Ditch
+print("Hi Eman!")
+shopping_schedule = int(input("Enter the timings in which tou wanna do shopping: "))
+print("So the total time we have for shopping is {shoppping_schedule}: ")
+brands_name = ("Enter the name of the brands from which you wanna do shopping: ")
+if shopping_schedule will work:
+print("Congratulations!"You are really good at time management")
+else:
+print("Dear!It seems like you find it really hard to choose one")
+
+print(''Happy Shopping")   
