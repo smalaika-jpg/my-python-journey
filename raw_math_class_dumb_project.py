ee850a9667859cba6fb24a79c19f2e24aa14ca98
smalaika-jpg <smalaika0609@gmail.com>
2026-07-19 00:29:47 +0500

    upload raw math class dumb project
    this a simple and interactive way of using python to help students learn BODMAS and left to right math opeartions

---
+++ raw_math_class_dumb_project.py
@@ -0,0 +1,20 @@
+    # MATH CLASS DUMB
+print("Hi students!")
+greetings = input("Enter your greeting's reply: ")
+print("So today we are going to learn BODMAS'')
+your­­_remarks = input("Enter are you excited (yes/no): ")
+print("We have six letters indicating six mathematical terms.")
+BODMAS = ["Brackets" ,"Of" ,"Division" ,"Multiply" ,"Addition" ,"Subtraction"]
+print("These are the main terms which tell how to solve any equation.")
+print("We have to follow the exact pattern to get the correct answer.However,When we just have the last two terms like in an equation we have subtraction and addition terms so we apply basic rule that indicates we have start from left and then go right telling rather addition or subtraction which has the left side that operation must be done first.")
+if left side == ''Addition sign" :
+print("Addition opeartion must be done first then.")
+elif left side == "Subtraction sign" :
+print("In such case,subtraction operation must be done first.")
+def math_problem() :
+   math_problem = equation(6+2-1)
+     return 7
+print(f"This {math_problem} has 7 solution.")
+math_problem = equation
+
+print("That's it for today!") 
