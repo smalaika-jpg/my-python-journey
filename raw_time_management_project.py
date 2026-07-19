68bf7564be48df3975fac9d5b22affe1479cfc42
smalaika-jpg <smalaika0609@gmail.com>
2026-07-19 23:45:22 +0500

    upload raw time management project
    This is a raw time management script as I am learning to use Python in daily life's basic work

---
+++ raw_time_management_project.py
@@ -0,0 +1,20 @@
+print("Hi")
+def time_managent( ):
+      time = really important
+        return time
+print(f"today we learn about {time}.this is very important in life.")
+time_mangement = time
+print("You should be ready for every work according to your schedule of time management.")
+For i in range(2):
+     print("Check your time at morning and evening.")
+checking = morning and evening
+If checking == "morning"
+    print("Make sure to done your morning task on time.")
+Elif checking == "evening"
+     print("Make sure to done every evening task on time and sleep well.")
+try:
+    print("Success: Entry of time_management schedule dumb to diary.txt.")
+except:
+    print("Error:invalid response and schedule not save in diary.")
+
+print("Enough for today!Bye.)
