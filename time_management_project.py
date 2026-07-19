d4ffb1e08978f3e83c7e4b20873338b6ef3642ca
smalaika-jpg <smalaika0609@gmail.com>
2026-07-19 23:54:49 +0500

    upload time management project
    A more clear version of my raw time management project without bugs and user confirmation.

---
+++ time_management_project.py
@@ -0,0 +1,20 @@
+print("Hi")
+def time_managent( ):
+      time = "really important"
+        return time
+print(f"today we learn about {time_mangement()}.This is very important in life.")
+time_mangement = time
+print("You should be ready for every work according to your schedule of time management.")
+For i in range(2):
+     print("Check your time at morning and evening.")
+checking = "morning"
+if checking == "morning":
+    print("Make sure to done your morning task on time.")
+elif checking == "evening":
+     print("Make sure to done every evening task on time and sleep well.")
+try:
+    print("Success: Entry of time_management schedule dumb to diary.txt.")
+except:
+    print("Error:invalid response and schedule not save in diary.")
+
+print("Enough for today!Bye.") 
