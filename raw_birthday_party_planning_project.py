d7f7f1a1a2e5da3dca3d907b2a622e83db1fa75b
smalaika-jpg <smalaika0609@gmail.com>
2026-07-21 00:32:25 +0500

    upload raw birthday party planning project
    A way to use Python for birthday planning purposes.

---
+++ raw_birthday_party_planning_project.py
@@ -0,0 +1,29 @@
+print("Hi Ayesha! ")
+greetings = input("Enter your greeting's reply: ")
+print("We are planning to through a party to Laila")
+pov = input("Enter your point of view regarding this")
+print("We are really glad that you like it too.Lets find a good gift.")
+if pov == "yes":
+    print("You should be with us as we are going to a shopping mall")
+elif pov =="no":
+    print("We are going to a shopping mall we will join u in a short time")
+else:
+    print("Errror! Invalid answer.")
+def shopping_haul:
+    shopping_haul: ("suit,heels,crockery")
+print(f"We are going to buy {shopping_haul}for Laila.")
+For i in range =<3:
+    print("We need to add something more in this")
+While range>3:
+   print("Stop! I guess this is enough!")
+   
+try:
+   with open("file.txt"."r") as my_file:
+       my_file.read("gift shopping")
+       my_file.read("Today's experience")
+ print("Success: storage of daily life routine in my file.txt")
+
+except:
+  print("Error:file didnt save in file.txt")
+
+print("That's all for today.") 
