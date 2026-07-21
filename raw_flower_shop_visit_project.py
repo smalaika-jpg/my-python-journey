8d3bec4311775dcf7a10927de578185cbe456dac
smalaika-jpg <smalaika0609@gmail.com>
2026-07-21 23:33:38 +0500

    upload raw flower shop visit project
    An idea to use python in daily life casual works

---
+++ raw_flower_shop_visit_project.py
@@ -0,0 +1,26 @@
+       # FLOWER SHOP VISIT
+print("Hey Alexa!")
+Greetings = input("Enter your greeting's reply")
+def asking_question:
+Offer = input("Actually today i want to go to a flowershop would you like to join me?: (yes/no) ")
+return time(1-2 hour)
+print(f"Maybe! It can take long than {"time"} like {time+2} hours")
+Friends_answer = input("Enter is it ok or not?")
+if Friends_answer = "Yes":
+print("Wow!I am too excited now!")
+elif:
+print("That's completely ok!Would love to go with you next time")
+else:
+print("Error!Invalid answer")
+print("Do you know!I m planning to buy which one flower?")
+flower_guess = input("Enter the name of flower you can guess according to your friend mood")
+flower_character = {
+     "name" = "tulip"
+     "color" = "white"
+"condition" = "fully bloom"
+}
+print(f"I love {"flower_character"}")
+opinion = input("Enter your opinion related your friends flower choice")
+
+print("I think its time to go")
+check it like my mentor of coding
